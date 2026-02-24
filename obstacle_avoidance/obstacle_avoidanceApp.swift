1017b66f9c1bb0b05572efb2c0d5812372b013a6
dtruong44 <dtruong@sandiego.edu>
2026-02-24 15:01:48 -0800

    remove login necessity

--- obstacle_avoidance/obstacle_avoidanceApp.swift
+++ obstacle_avoidance/obstacle_avoidanceApp.swift
@@ -12,11 +12,5 @@ struct ObstacleAvoidanceApp: App {
     @AppStorage("isLoggedIn") private var isLoggedIn = false
     var body: some Scene {
         WindowGroup {
-                    if isLoggedIn {
-                        ContentView()  // Show main app if logged in
-                    } else {
-                        LoginView()  // Show login screen if not logged in
-                    }
-                }
-    }
+            ContentView()  // Show main app if logged in
 }
